750a990847f50b7920ee83efaa615bb0f66f82bb
Miladul Islam <imiladul@gmail.com>
2021-07-25 10:19:14 +0600

    Courses CRUD API remove from org management

--- routes/web.php
+++ routes/web.php
@@ -47,8 +47,6 @@ $router->group(['prefix' => 'api/v1', 'as' => 'api.v1'], function () use ($route
     //Org Unit Statistic crud api
     $customRouter()->resourceRoute('organization-unit-services', 'OrganizationUnitServiceController')->render();
 
-    //Org Unit Statistic crud api
-    $customRouter()->resourceRoute('courses', 'CourseController')->render();
 
 
 });
