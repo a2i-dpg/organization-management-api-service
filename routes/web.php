c9d15edaec6ee71c38765c7fffb595882cc2e630
Miladul Islam <imiladul@gmail.com>
2021-07-27 16:51:31 +0600

    Route fix

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,6 @@ $router->group(['prefix' => 'api/v1', 'as' => 'api.v1'], function () use ($route
     $customRouter()->resourceRoute('occupations', 'OccupationController')->render();
     $customRouter()->resourceRoute('organization-types', 'OrganizationTypeController')->render();
     $customRouter()->resourceRoute('organizations', 'OrganizationController')->render();
-    $customRouter()->resourceRoute('organizations', 'OrganizationController')->render();
     $customRouter()->resourceRoute('organization-unit-types', 'OrganizationUnitTypeController')->render();
     $customRouter()->resourceRoute('human-resource-templates', 'HumanResourceTemplateController')->render();
     $customRouter()->resourceRoute('human-resources', 'HumanResourceController')->render();
