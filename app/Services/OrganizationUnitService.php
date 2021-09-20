6da6d467d181293633ba2070cd46e924d024463d
taskinur rahman <taskinur.rahman2014@gmail.com>
2021-09-20 12:31:53 +0600

    updated getList filter

--- app/Services/OrganizationUnitService.php
+++ app/Services/OrganizationUnitService.php
@@ -26,12 +26,14 @@ class OrganizationUnitService
      */
     public function getAllOrganizationUnit(array $request, Carbon $startTime): array
     {
-        $titleEn = array_key_exists('title_en', $request) ? $request['title_en'] : "";
-        $titleBn = array_key_exists('title_bn', $request) ? $request['title_bn'] : "";
-        $paginate = array_key_exists('page', $request) ? $request['page'] : "";
-        $pageSize = array_key_exists('page_size', $request) ? $request['page_size'] : "";
-        $rowStatus = array_key_exists('row_status', $request) ? $request['row_status'] : "";
-        $order = array_key_exists('order', $request) ? $request['order'] : "ASC";
+        $titleEn = $request['title_en'] ?? "";
+        $titleBn = $request['title_bn'] ?? "";
+        $paginate = $request['page'] ?? "";
+        $pageSize = $request['page_size'] ?? "";
+        $rowStatus = $request['row_status'] ?? "";
+        $order = $request['order'] ?? "ASC";
+        $organizationId = $request['organization_id'] ?? "ASC";
+        $organizationUnitTypeId = $request['organization_unit_type_id'] ?? "ASC";
 
         /** @var Builder $organizationUnitBuilder */
         $organizationUnitBuilder = OrganizationUnit::select([
@@ -109,6 +111,12 @@ class OrganizationUnitService
 
         if (is_numeric($rowStatus)) {
             $organizationUnitBuilder->where('organization_units.row_status', $rowStatus);
+        }
+        if (is_numeric($organizationId)) {
+            $organizationUnitBuilder->where('organization_units.organization_id', $organizationId);
+        }
+        if (is_numeric($organizationUnitTypeId)) {
+            $organizationUnitBuilder->where('organization_units.organization_unit_type_id', $organizationUnitTypeId);
         }
         if (!empty($titleEn)) {
             $organizationUnitBuilder->where('organization_units.title_en', 'like', '%' . $titleEn . '%');
@@ -511,6 +519,8 @@ class OrganizationUnitService
             'title_bn' => 'nullable|min:1',
             'page' => 'numeric|gt:0',
             'pageSize' => 'numeric',
+            'organization_id' => 'numeric|gt:0',
+            'organization_unit_type_id' => 'numeric|gt:0',
             'order' => [
                 'string',
                 Rule::in([BaseModel::ROW_ORDER_ASC, BaseModel::ROW_ORDER_DESC])
