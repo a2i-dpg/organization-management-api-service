cde86735b92a2270730fa435570f78ee590dba37
rrahul <rahulbgc21@gmail.com>
2021-09-28 17:35:48 +0600

    organization auth user stuff

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\User;
+use App\Services\UserRolePermissionManagementServices\UserService;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -30,10 +32,23 @@ class AuthServiceProvider extends ServiceProvider
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
 
-        $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+        $this->app['auth']->viaRequest('token', function ($request) {
+            $token = $request->header('Token');
+            $authUser = null;
+            if ($token) {
+                $header = explode(" ", $token);
+                if (count($header) > 1) {
+                    $tokenParts = explode(".", $header[1]);
+                    if (count($tokenParts) == 3) {
+                        $tokenPayload = base64_decode($tokenParts[1]);
+                        $jwtPayload = json_decode($tokenPayload);
+                        $userService = $this->app->make(UserService::class);
+                        $authUser = $userService->getAuthPermission($jwtPayload->sub ?? null);
+                    }
+                }
+                Log::info("userInfoWithIdpId:" . json_encode($authUser));
             }
+            return $authUser;
         });
     }
 }
