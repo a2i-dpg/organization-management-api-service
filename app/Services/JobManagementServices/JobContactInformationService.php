20454df3108650c66dcdb58abbd525fd12d345ac
Md Tasmidur Rahman <tasmidurrahman@gmail.com>
2021-12-28 10:08:20 +0600

    update logging config file

--- app/Services/JobManagementServices/JobContactInformationService.php
+++ app/Services/JobManagementServices/JobContactInformationService.php
@@ -3,19 +3,50 @@
 namespace App\Services\JobManagementServices;
 
 
+use App\Models\JobContactInformation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class JobContactInformationService
 {
 
-    public function validate(Request $request)
+
+    /**
+     * @param string $jobId
+     * @return JobContactInformation
+     */
+    public function getContactInformation(string $jobId): JobContactInformation
+    {
+        return JobContactInformation::where('job_id', $jobId)->firstOrFail();
+    }
+
+    /**
+     * @param array $data
+     */
+    public function store(array $data)
+    {
+        $jobInformation = new JobContactInformation();
+        $jobInformation->fill($data);
+        $jobInformation->save();
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    public function validate(Request $request): \Illuminate\Contracts\Validation\Validator
     {
         $rules = [
             "job_id" => [
                 "required",
-                "exists:primary_job_information,job_id"
+//                "exists:primary_job_information,job_id"
             ],
+            "contact_person_id" => [
+                "required"
+            ]
         ];
+
+        return Validator::make($request->all(), $rules);
 
     }
 }
