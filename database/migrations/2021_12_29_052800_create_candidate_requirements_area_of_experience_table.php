dda8e09ac7cf920ec924b6aa981f552b1326b544
rrahul <rahulbgc21@gmail.com>
2021-12-29 17:24:36 +0600

    refactoring

--- database/migrations/2021_12_29_052800_create_candidate_requirements_area_of_experience_table.php
+++ database/migrations/2021_12_29_052800_create_candidate_requirements_area_of_experience_table.php
@@ -15,7 +15,7 @@ class CreateCandidateRequirementsAreaOfExperienceTable extends Migration
     {
         Schema::create('candidate_requirements_area_of_experience', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer("candidate_requirements_id")->index();
+            $table->integer("candidate_requirements_id")->index('index_area_experience_can_req_id');
             $table->integer("area_of_experience_id")->index();
             $table->timestamps();
         });
