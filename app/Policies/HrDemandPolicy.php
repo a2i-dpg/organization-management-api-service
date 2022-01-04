349e8b16c928514bb424b3795eb3bbe8dfbf1d04
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2022-01-04 14:40:01 +0600

    Added PUT operation for HrDemand with Policy

--- app/Policies/HrDemandPolicy.php
+++ app/Policies/HrDemandPolicy.php
@@ -5,11 +5,32 @@ namespace App\Policies;
 use App\Models\HrDemand;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
-use Illuminate\Support\Facades\Log;
 
 class HrDemandPolicy
 {
     use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any HrDemand.
+     *
+     * @param User $authUser
+     * @return bool
+     */
+    public function viewAny(User $authUser): bool
+    {
+        return $authUser->hasPermission('view_any_hr_demand');
+    }
+
+    /**
+     * Determine whether the user can view the HrDemand.
+     *
+     * @param User $authUser
+     * @return bool
+     */
+    public function view(User $authUser): bool
+    {
+        return $authUser->hasPermission('view_single_hr_demand');
+    }
 
     /**
      * Determine whether the user can create HrDemand.
@@ -23,14 +44,24 @@ class HrDemandPolicy
     }
 
     /**
-     * Determine whether the user can update the humanResource.
+     * Determine whether the user can update the HrDemand.
      *
      * @param User $authUser
-     * @param HrDemand $hrDemand
      * @return bool
      */
-    public function update(User $authUser, HrDemand $hrDemand): bool
+    public function update(User $authUser): bool
     {
         return $authUser->hasPermission('update_hr_demand');
+    }
+
+    /**
+     * Determine whether the user can delete the HrDemand.
+     *
+     * @param User $authUser
+     * @return bool
+     */
+    public function delete(User $authUser): bool
+    {
+        return $authUser->hasPermission('delete_hr_demand');
     }
 }
