a0eba9a76805e119468ea30838dc7b8ae0f31d0a
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2022-01-30 15:29:08 +0600

    Added PIVOT table for Hr demand Skill

--- database/migrations/2022_01_27_043239_create_hr_demand_skills_table.php
+++ database/migrations/2022_01_27_043239_create_hr_demand_skills_table.php
@@ -14,13 +14,9 @@ class CreateHrDemandSkillsTable extends Migration
     public function up()
     {
         Schema::create('hr_demand_skills', function (Blueprint $table) {
-            $table->increments('id');
             $table->unsignedInteger("hr_demand_id");
-            $table->unsignedTinyInteger("skill_type")->comment('1 => Mandatory, 2 => Optional')->default(1);
             $table->unsignedInteger("skill_id");
-            $table->unsignedTinyInteger('row_status')->default(1)->comment('0 => inactive, 1 => active, 2 => invalid');
-            $table->timestamps();
-            $table->softDeletes();
+            $table->unsignedTinyInteger("skill_type")->comment('1 => Mandatory, 2 => Optional')->default(1);
         });
     }
 
