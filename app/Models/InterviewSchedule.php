159f682fec6dd1ce9268fb331cf2103187c10e20
mohammad.abdur.nur.al.maruf@softbdltd.com <mohammad.abdur.nur.al.maruf@softbdltd.com>
2022-02-01 12:48:10 +0600

    assign to schedule

--- app/Models/InterviewSchedule.php
+++ app/Models/InterviewSchedule.php
@@ -9,4 +9,18 @@ class InterviewSchedule extends BaseModel
 {
     use SoftDeletes;
     protected $guarded = BaseModel::COMMON_GUARDED_FIELDS_SOFT_DELETE;
+
+
+    public const NOT_CONFIRMED = 0;
+    public const CONFIRMED = 1;
+
+    public const REQUEST_RESCHEDULED = 2;
+    public const ABORTED = 3;
+
+    public const CONFIRMATION_STATUS = [
+        self::ROW_STATUS_ACTIVE,
+        self::ROW_STATUS_INACTIVE,
+        self::ROW_STATUS_PENDING,
+        self::ROW_STATUS_REJECTED,
+    ];
 }
