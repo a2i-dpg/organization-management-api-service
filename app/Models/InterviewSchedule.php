85ab382986ab34c295c1870fae13c122895919e4
mohammad.abdur.nur.al.maruf@softbdltd.com <mohammad.abdur.nur.al.maruf@softbdltd.com>
2022-02-01 14:12:47 +0600

    /hr-demand-youths/{hr_demand_institute_id} route added in doc

--- app/Models/InterviewSchedule.php
+++ app/Models/InterviewSchedule.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class InterviewSchedule extends BaseModel
@@ -23,4 +24,10 @@ class InterviewSchedule extends BaseModel
         self::ROW_STATUS_PENDING,
         self::ROW_STATUS_REJECTED,
     ];
+
+
+    public function jobApplicants(): BelongsToMany
+    {
+        return $this->belongsToMany(AppliedJob::class, 'assign_candidates_to_schedules', 'applied_job_id', 'id');
+    }
 }
