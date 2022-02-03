34149a22a842bbcb1a305ddb2a69080f065ebace
ASM Mahmudul Hasan <asmmahmud@gmail.com>
2022-02-03 15:32:20 +0600

    small change in seeder file

--- app/Http/Controllers/IndustryAssociationController.php
+++ app/Http/Controllers/IndustryAssociationController.php
@@ -14,7 +14,6 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Client\RequestException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Validation\ValidationException;
