5ba4c4bd58c3ceba4a8afb83752bbf1963ad3aad
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2022-02-06 20:36:42 +0600

    Added single HrDemandYouth rejection API

--- app/Http/Controllers/HrDemandYouthController.php
+++ app/Http/Controllers/HrDemandYouthController.php
@@ -5,12 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\HrDemandYouth;
 use App\Services\HrDemandYouthService;
 use Carbon\Carbon;
-use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+use Throwable;
 
 class HrDemandYouthController extends Controller
 {
@@ -38,10 +38,41 @@ class HrDemandYouthController extends Controller
      */
     public function getHrDemandYouths(Request $request, int $hr_demand_institute_id): JsonResponse
     {
-//        $this->authorize('viewAny', HrDemandYouth::class);
+        //$this->authorize('viewAny', HrDemandYouth::class);
 
         $filter = $this->hrDemandYouthService->filterValidator($request)->validate();
         $response = $this->hrDemandYouthService->getHrDemandYouthList($filter, $this->startTime, $hr_demand_institute_id);
+
+        return Response::json($response, ResponseAlias::HTTP_OK);
+    }
+
+    /**
+     * Display a listing of the Hr Demand Youths.
+     *
+     * @param int $hr_demand_youth_id
+     * @return JsonResponse
+     * @throws Throwable
+     */
+    public function deleteHrDemandYouth(int $hr_demand_youth_id): JsonResponse
+    {
+        //$this->authorize('delete', HrDemandYouth::class);
+
+        $hrDemandYouth = HrDemandYouth::findOrFail($hr_demand_youth_id);
+
+        throw_if($hrDemandYouth->row_status == HrDemandYouth::ROW_STATUS_INVALID, ValidationException::withMessages([
+            "Hr Demand Youth already Invalidated by Institute User!"
+        ]));
+
+        $this->hrDemandYouthService->deleteHrDemandYouth($hrDemandYouth);
+
+        $response = [
+            '_response_status' => [
+                "success" => true,
+                "code" => ResponseAlias::HTTP_OK,
+                "message" => "HR Demand Youth Rejected Successfully.",
+                "query_time" => $this->startTime->diffInSeconds(Carbon::now()),
+            ]
+        ];
 
         return Response::json($response, ResponseAlias::HTTP_OK);
     }
