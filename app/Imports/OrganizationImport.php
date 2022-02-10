63c0b1c1d61d35a46a629b8accf7203859b1683f
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2022-02-10 11:09:26 +0600

    Solved to catch header from excel with validation

--- app/Imports/OrganizationImport.php
+++ app/Imports/OrganizationImport.php
@@ -3,19 +3,42 @@
 namespace App\Imports;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rule;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
 
-class OrganizationImport implements ToCollection
+class OrganizationImport implements ToCollection, WithValidation, WithHeadingRow
 {
+    /**
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => function($attribute, $value, $onFailure) {
+                Log::info("Name ********************** 11111111");
+                Log::info($attribute);
+                Log::info($value);
+            },
+            'contact_person_name' => function($attribute, $value, $onFailure) {
+                Log::info("contact ********************** 2222222222222");
+                Log::info($attribute);
+                Log::info($value);
+            },
+            'mobile' => Rule::in(['1674248402'])
+        ];
+    }
+
     /**
      * @param Collection $rows
      */
     public function collection(Collection $rows)
     {
-        dd($rows);
-        foreach ($rows as $row)
-        {
-            dd($row);
+        foreach ($rows as $row) {
+            Log::info("Value >>>>>> Value <<<<<<<<<<");
+            Log::info(json_encode($row));
         }
     }
 }
