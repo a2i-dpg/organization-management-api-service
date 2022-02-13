dd3fe74733bd46e4e15a3106122a553c66ea8756
mohammad.abdur.nur.al.maruf@softbdltd.com <mohammad.abdur.nur.al.maruf@softbdltd.com>
2022-02-13 18:09:03 +0600

    deschedule

--- app/Services/InterviewScheduleService.php
+++ app/Services/InterviewScheduleService.php
@@ -170,6 +170,10 @@ class InterviewScheduleService
      */
     public function CandidateRemoveFromScheduleValidator(Request $request, InterviewSchedule $interviewSchedule): \Illuminate\Contracts\Validation\Validator
     {
+        if (!empty($request['applied_job_ids'])) {
+            $request['applied_job_ids'] = isset($request['applied_job_ids']) && is_array($request['applied_job_ids']) ? $request['applied_job_ids'] : explode(',', $request['applied_job_ids']);
+        }
+
         $rules = [
             'applied_job_ids' => [
                 'required',
