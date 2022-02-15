c5f44718d5937d400eed8d7ed965c043e35e6b7f
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2022-02-15 12:04:33 +0600

    Solved issue for hr demand institute get list from institute user

--- app/Services/HrDemandInstituteService.php
+++ app/Services/HrDemandInstituteService.php
@@ -140,6 +140,7 @@ class HrDemandInstituteService
         Log::info(json_encode($hrDemandInstitutes));
         Log::info("The output will be: ");
         Log::info(json_encode(!isset($hrDemandInstitutes->toArray()['data']) ? array_values($hrDemandInstitutes->toArray()['data']) : array_values($hrDemandInstitutes->toArray())));
+        Log::info("The data is here: " . json_encode(isset($hrDemandInstitutes->toArray()['data'])));
 
         $response['order'] = $order;
         $response['data'] = !empty($hrDemandInstitutes->toArray()['data']) ? array_values($hrDemandInstitutes->toArray()['data']) : array_values($hrDemandInstitutes->toArray());
