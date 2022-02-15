35d95259e24e4f570651a927d9cad3682d47218d
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2022-02-15 16:53:03 +0600

    RELEASE = 'php artisan list'

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -214,7 +214,7 @@ class OrganizationController extends Controller
         $organization = app(Organization::class);
         $this->authorize('create', $organization);
 
-//        $this->organizationService->excelImportValidator($request)->validate();
+        //$this->organizationService->excelImportValidator($request)->validate();
 
         $file = $request->file('file');
         Excel::import(new OrganizationImport(), $file);
