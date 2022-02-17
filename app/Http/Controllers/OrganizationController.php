e9669b3cfc72ebe68305f06e446485b33233dfb5
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2022-02-17 18:32:12 +0600

    modify for error uername

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -224,6 +224,7 @@ class OrganizationController extends Controller
         $file = $request->file('file');
         $excelData = Excel::toCollection(new OrganizationImport(), $file)->toArray();
         $alreadyExistUsernames = [];
+        $errorOccurUsernames = [];
         $organizationCreated = 0;
 
         if (!empty($excelData) && !empty($excelData[0])) {
@@ -285,7 +286,7 @@ class OrganizationController extends Controller
                     } catch (Throwable $e) {
                         Log::info("Error occurred. Inside catch block. Error is: " . json_encode($e->getMessage()));
                         DB::rollBack();
-                        throw $e;
+                        $errorOccurUsernames[] = $rowData['contact_person_mobile'];
                     }
                 } else {
                     $alreadyExistUsernames[] = $rowData['contact_person_mobile'];
@@ -303,6 +304,10 @@ class OrganizationController extends Controller
 
         if (!empty($alreadyExistUsernames)) {
             $response['_response_status']['user_exists'] = $alreadyExistUsernames;
+        }
+
+        if (!empty($errorOccurUsernames)) {
+            $response['_response_status']['error_occur_username'] = $errorOccurUsernames;
         }
 
         return Response::json($response, ResponseAlias::HTTP_CREATED);
