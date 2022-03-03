9c1b4e5c868099c47148e37b19147743f06828a9
ASM Mahmudul Hasan <asmmahmud@gmail.com>
2022-03-03 11:51:02 +0600

    config kog generate

--- database/seeders/IndustryAssociationConfigSeeder.php
+++ database/seeders/IndustryAssociationConfigSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\IndustryAssociationConfig;
-use App\Models\MembershipType;
 use App\Models\PaymentTransactionHistory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Schema;
