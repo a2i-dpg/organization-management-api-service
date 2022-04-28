fd71af46360a9244219bc85e5dcee31d2ca6933d
golam.kibria@softbdltd.com <kibria7533@A>
2022-04-28 14:28:28 +0600

    Guideline doc added

---
+++ database/migrations/2022_04_26_065601_create_four_ir_showcasings_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFourIrShowcasingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('four_ir_showcasings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('four_ir_project_id');
+            $table->string('occupation_name', 100);
+            $table->string('occupation_name_en', 100)->nullable();
+            $table->date('date');
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+            $table->string('venue', 100);
+            $table->string('venue_en', 100)->nullable();
+            $table->string('invite_others', 100);
+            $table->string('accessor_type', 100);
+            $table->unsignedInteger('accessor_id');
+            $table->unsignedTinyInteger('row_status')->default(1)->comment('0 => inactive, 1 => active');
+            $table->unsignedInteger('created_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('four_ir_showcasings');
+    }
+}
