46ab1d8cda51d32b4214cf0e11249e649507d66a
golam.kibria@softbdltd.com <kibria7533@A>
2022-05-12 11:12:57 +0600

    Updated Resource Api

--- app/Http/Controllers/FourIRResourceController.php
+++ app/Http/Controllers/FourIRResourceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\FourIRInitiative;
 use App\Models\FourIRResource;
+use App\Models\FourIRSector;
 use App\Services\FourIRServices\FourIRFileLogService;
 use App\Services\FourIRServices\FourIRResourceService;
 use Carbon\Carbon;
@@ -37,6 +38,20 @@ class FourIRResourceController extends Controller
 
 
     /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function getList(Request $request): JsonResponse
+    {
+
+
+        $filter = $this->fourIRResourceService->filterValidator($request)->validate();
+        $response = $this->fourIRResourceService->getResourceList($filter, $this->startTime);
+        return Response::json($response,ResponseAlias::HTTP_OK);
+    }
+
+    /**
      * Only one resource_management can be for an initiative. That's why only single read API is here.
      * Provide Initiative id as the path parameter of this API
      *
@@ -59,6 +74,28 @@ class FourIRResourceController extends Controller
             ]
         ];
         return Response::json($response,ResponseAlias::HTTP_OK);
+    }
+
+    /**
+     * @throws Throwable
+     * @throws ValidationException
+     */
+    public function update(Request $request, int $id): JsonResponse
+    {
+        $resource = FourIRResource::query()->findOrFail($id);
+
+        $validated = $this->fourIRResourceService->validator($request, $id)->validate();
+        $data = $this->fourIRResourceService->update($resource, $validated);
+        $response = [
+            'data' => $data,
+            '_response_status' => [
+                "success" => true,
+                "code" => ResponseAlias::HTTP_OK,
+                "message" => "FourIRSector updated successfully.",
+                "query_time" => $this->startTime->diffInSeconds(Carbon::now())
+            ]
+        ];
+        return Response::json($response, ResponseAlias::HTTP_CREATED);
     }
 
     /**
