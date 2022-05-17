1beb5313b902c780252d6490d3921fdb603eced4
tasmidur <tasmidurrahman@gmail.com>
2022-05-17 17:53:55 +0600

    Show Casing Ready  list ready

---
+++ app/Models/FourIRContribution.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models;
+
+class FourIRContribution extends BaseModel
+{
+    protected $table = "four_ir_contributions";
+    protected $guarded = BaseModel::COMMON_GUARDED_FIELDS_NON_SOFT_DELETE;
+}
