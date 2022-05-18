8ae3d45586681d9cb12a8bb280defe6331fbe2cb
Asir053 <asirbillah@gmail.com>
2022-05-18 17:01:06 +0600

    changed titles in other benefit seeder

--- database/seeders/OtherBenefitSeeder.php
+++ database/seeders/OtherBenefitSeeder.php
@@ -21,18 +21,18 @@ class OtherBenefitSeeder extends Seeder
 
         $otherBenefits = [
             array('id' => '1', 'title' => 'T/A', 'title_en' => 'T/A', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
-            array('id' => '2', 'title' => 'Mobile bill', 'title_en' => 'Mobile bill', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
-            array('id' => '3', 'title' => 'Pension policy', 'title_en' => 'Pension policy', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
-            array('id' => '4', 'title' => 'Tour allowance', 'title_en' => 'Tour allowance', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
-            array('id' => '5', 'title' => 'Credit card', 'title_en' => 'Credit card', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
-            array('id' => '6', 'title' => 'Medical allowance', 'title_en' => 'Medical allowance', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
-            array('id' => '7', 'title' => 'Performance bonus', 'title_en' => 'Performance bonus', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
-            array('id' => '8', 'title' => 'Profit share', 'title_en' => 'Profit share', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
-            array('id' => '9', 'title' => 'Provident fund', 'title_en' => 'Provident fund', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
-            array('id' => '10', 'title' => 'Weekly 2 holidays', 'title_en' => 'Weekly 2 holidays', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
-            array('id' => '11', 'title' => 'Insurance', 'title_en' => 'Insurance', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
-            array('id' => '12', 'title' => 'Gratuity', 'title_en' => 'Gratuity', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
-            array('id' => '13', 'title' => 'Over time allowance', 'title_en' => 'Over time allowance', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL)
+            array('id' => '2', 'title' => 'মোবাইল বিল', 'title_en' => 'Mobile bill', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
+            array('id' => '3', 'title' => 'পেনশন পলিসি', 'title_en' => 'Pension policy', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
+            array('id' => '4', 'title' => 'সফর ভাতা', 'title_en' => 'Tour allowance', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
+            array('id' => '5', 'title' => 'ক্রেডিট কার্ড', 'title_en' => 'Credit card', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
+            array('id' => '6', 'title' => 'চিকিৎসা ভাতা', 'title_en' => 'Medical allowance', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
+            array('id' => '7', 'title' => 'পারফর্ম্যান্স বোনাস', 'title_en' => 'Performance bonus', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
+            array('id' => '8', 'title' => 'প্রফিট শেয়ার', 'title_en' => 'Profit share', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
+            array('id' => '9', 'title' => 'প্রভিডেন্ট ফান্ড', 'title_en' => 'Provident fund', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
+            array('id' => '10', 'title' => 'সাপ্তাহিক দুইদিন ছুটি', 'title_en' => 'Weekly 2 holidays', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
+            array('id' => '11', 'title' => 'বীমা', 'title_en' => 'Insurance', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
+            array('id' => '12', 'title' => 'গ্র্যাচুইটি', 'title_en' => 'Gratuity', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL),
+            array('id' => '13', 'title' => 'ওভার টাইম এলাউন্স', 'title_en' => 'Over time allowance', 'deleted_at' => NULL, 'created_at' => NULL, 'updated_at' => NULL)
         ];
 
         OtherBenefit::insert($otherBenefits);
