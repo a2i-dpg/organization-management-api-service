23db8dab5c491360f334d8905a45b657b663a6dd
tasmidur <tasmidurrahman@gmail.com>
2022-05-19 18:11:56 +0600

    application_role_id set to role Id

--- app/Http/Controllers/FourIRContributionController.php
+++ app/Http/Controllers/FourIRContributionController.php
@@ -28,8 +28,6 @@ class FourIRContributionController extends Controller
 
     public function getList(Request $request): JsonResponse
     {
-        Log::info("Auth UserId: " . Auth::id());
-
         $filter = $this->fourIRContributionService->filterValidator($request)->validate();
         $response = $this->fourIRContributionService->getContributionList($filter);
         return Response::json($response, $response['_response_status']['code']);
