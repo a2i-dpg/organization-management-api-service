5389f7099b80dfefb1e52270ea04ad8519e646eb
golam.kibria@softbdltd.com <kibria7533@A>
2022-05-21 17:01:06 +0600

    empty check added

---
+++ database/migrations/2022_05_21_105655_modify_sector_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class ModifySectorIdField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE four_ir_initiative_cs_curriculum_cblm CHANGE sector_id sector_name INT(10) UNSIGNED');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
