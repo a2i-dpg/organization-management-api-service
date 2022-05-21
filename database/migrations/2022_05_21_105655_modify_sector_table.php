d804bc74d8f045dc655dcf5b352f1e3abe07b3ef
golam.kibria@softbdltd.com <kibria7533@A>
2022-05-21 17:05:42 +0600

    empty check added

--- database/migrations/2022_05_21_105655_modify_sector_table.php
+++ database/migrations/2022_05_21_105655_modify_sector_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class ModifySectorIdField extends Migration
+class ModifySectorTable extends Migration
 {
     /**
      * Run the migrations.
