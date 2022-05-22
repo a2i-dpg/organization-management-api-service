4b9fd081e0d117e202919666055c300aa5ef60c9
golam.kibria@softbdltd.com <kibria7533@A>
2022-05-22 15:17:35 +0600

    empty check added

--- app/Traits/Scopes/ScopeAcl.php
+++ app/Traits/Scopes/ScopeAcl.php
@@ -45,6 +45,7 @@ trait ScopeAcl
             if (Schema::hasColumn($tableName, 'accessor_id')) {
                 $query->where($tableName . '.accessor_id', $authUser->institute_id);
             }
+            Log::info('Institute Id'.$authUser->institute_id);
         } else if ($authUser && $authUser->isRtoUser()) {  // RTO User
             if (Schema::hasColumn($tableName, 'registered_training_organization_id')) {
                 $query = $query->where($tableName . '.registered_training_organization_id', $authUser->registered_training_organization_id);
